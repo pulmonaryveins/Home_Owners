7eee83ef7cff015c532b1cf44e5c14688c57fd88
Vince Bryant Cabunilas <vincebryant42@gmail.com>
2025-02-28 12:54:51 +0800

    Add project files.

---
+++ HomeOwners/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace HomeOwners.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    // Note: IdentityUser already includes Username property
+    // No need to add it explicitly
+}
+
---
+++ HomeOwners/Areas/Identity/Data/HomeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HomeOwners.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeOwners.Data;
+
+public class HomeDbContext : IdentityDbContext<ApplicationUser>
+{
+    public HomeDbContext(DbContextOptions<HomeDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
